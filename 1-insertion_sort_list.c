c5b147a0196a3ded125ba7b54d4ab369f36b28d6
Anselm-Chidera <okaforchidera2000@gmail.com>
2022-09-18 17:07:15 -0700

    sorting alhorithms

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - A function that sorts a doubly linked list
+ * in ascending order using the insertion sort algorithm
+ *
+ * @list: The list to sort
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node, *back, *temp, *temp2;
+
+	node = *list;
+	if (node->next == NULL || node == NULL)
+		return;
+	while (node != NULL)
+	{
+		back = node;
+		node = node->next;
+		if (node->n < back->n)
+		{
+			temp = node->next;
+			temp2 = back->prev;
+			node->next = back;
+			back->prev = node;
+			if (temp2 != NULL)
+			{
+				node->prev = temp2;
+				temp2->next = node;
+			}
+			else
+			{
+				node->prev = NULL;
+				*list = node;
+			}
+			if (temp != NULL)
+			{
+				back->next = temp;
+				temp->prev = back;
+			}
+			else
+				back->next = NULL;
+			print_list(*list);
+			while (node->prev != NULL)
+				node = node->prev;
+		}
+		if (node->next == NULL)
+			break;
+	}
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * quick_sort - A function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm.
+ * It implements the Lomuto partition scheme
+ *
+ * @array: The array to be sorted
+ * @size: the size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	int last = size - 1, pivot, n, idx, temp;
+
+	if (size < 2)
+		return;
+	while (last != 0)
+	{
+		n = 0;
+		pivot = array[last];
+		while (n < last)
+		{
+			if (array[n] > pivot)
+			{
+				/* set a flag on array[n]*/
+				idx = n;
+				break;
+			}
+			n++;
+		}
+		if (n != last)
+		{
+			while (n != last)
+			{
+				n++;
+				if (array[n] < pivot || n == last)
+				{
+					temp = array[n];
+					array[n] = array[idx];
+					array[idx] = temp;
+					print_array(array, size);
+					break;
+				}
+			}
+		}
+		else
+			last -= 1;
+	}
+}
